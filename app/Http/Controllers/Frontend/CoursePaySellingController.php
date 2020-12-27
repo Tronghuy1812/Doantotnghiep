007271a8cb02b77d52b05ab473472737a25ed40b
Phan Trung Phú <phupt@dol-tech.com>
2020-12-27 15:23:58 +0700

    add khoa hoc ban chaay

--- app/Http/Controllers/Frontend/CoursePaySellingController.php
+++ app/Http/Controllers/Frontend/CoursePaySellingController.php
@@ -8,18 +8,32 @@ use Illuminate\Http\Request;
 
 class CoursePaySellingController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         \SEOMeta::setTitle('Khoá học bán chạy');
 
         $courses = Course::with('teacher:id,t_name,t_avatar,t_slug,t_job')
-            ->where('c_status',Course::STATUS_DEFAULT)
-            ->where('c_total_pay','>',0)
-            ->orderBy('c_total_pay','desc')
-            ->paginate(12);
+            ->where('c_status', Course::STATUS_DEFAULT)
+            ->where('c_total_pay', '>', 0);
+
+
+        if ($lv = $request->level) $courses->where('c_level', $lv);
+        if ($time = $request->time) {
+            if ($time == 1) {
+                $courses->where('c_total_time', '<=', 3);
+            } else {
+                $courses->where('c_total_time', '>', 3);
+            }
+        }
+        $courses = $courses->orderBy('c_total_pay', 'desc')
+        ->paginate(12);
+
+        $level = (new Course())->level;
 
         $viewData = [
-            'courses' => $courses
+            'courses' => $courses,
+            'level'   => $level,
+            'query'   => $request->query()
         ];
 
         return view('pages.pay_selling.index', $viewData);
